9fa23c7c0e8d4d24953c6c228308771121698e5a
tomokotomako <zarganinoakakun@gmail.com>
2026-04-21 22:54:23 -0700

    Update main.py

--- main.py
+++ main.py
@@ -53,7 +53,7 @@ def handle_image(event):
 """
 
     response = client.models.generate_content(
-        model='gemini-2.0-flash-lite',
+        model='gemini-1.5-flash-latest',
         contents=[
             types.Content(
                 role='user',
